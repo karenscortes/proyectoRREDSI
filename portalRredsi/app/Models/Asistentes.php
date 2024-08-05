769dc5fef949d6348df80019a03396ffcbdedfb9
Maribel Obando Joven <maribel-obando.j@utp.edu.co>
2024-08-05 14:37:04 -0500

    add: Creacion de migracion y modelos

---
+++ portalRredsi/app/Models/Asistentes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Asistentes extends Model
+{
+    use HasFactory;
+}
---
+++ portalRredsi/database/migrations/2024_08_05_190718_create_permisos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permisos', function (Blueprint $table) {
+            $table->unsignedInteger("id_modulo");
+            $table->unsignedInteger("id_rol");
+
+            $table->primary(['id_rol', 'id_modulo']);
+
+        
+            $table->foreign('id_rol')
+                  ->references('id_rol')->on('rols')
+                  ->onDelete('cascade');
+            
+            $table->foreign('id_modulo')
+                  ->references('id_modulo')->on('modulos')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permisos');
+    }
+};
