89334459c409da745977951729a15550a33d251a
Miguel-Alzate <miguelangel4817@gmail.com>
2024-08-10 18:30:52 -0500

    update: correcion de modelos

--- portalRredsi/api/appv1/models/respuesta_rubrica.py
+++ portalRredsi/api/appv1/models/respuesta_rubrica.py
@@ -2,13 +2,13 @@ from sqlalchemy import Column, Float, ForeignKey, Integer, Text
 from sqlalchemy.orm import relationship
 from models.base_class import Base
 
-class Respuesta_rubrica(Base):
-    __tablename__ = 'respuestas_rubrica'
+class Respuesta_rubricas(Base):
+    __tablename__ = 'respuestas_rubricas'
     id_respuestas_rubrica = Column(Integer, primary_key=True, autoincrement=True)
-    id_item_rubrica = Column(Integer, ForeignKey('item_rubrica.id_item_rubrica'))
-    id_rubrica_resultado = Column(Integer, ForeignKey('rubrica_resultado.id_rubrica_resultado'))
-    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'))
-    id_proyecto_convocatoria = Column(Integer, ForeignKey('proyecto_convocatoria.id_proyecto_convocatoria'))
+    id_item_rubrica = Column(Integer, ForeignKey('items_rubrica.id_item_rubrica'))
+    id_rubrica_resultado = Column(Integer, ForeignKey('rubricas_resultados.id_rubrica_resultado'))
+    id_usuario = Column(Integer, ForeignKey('usuarios.id_usuario'))
+    id_proyecto_convocatoria = Column(Integer, ForeignKey('proyectos_convocatoria.id_proyecto_convocatoria'))
     observacion = Column(Text)
     calificacion = Column(Float(2,1))
 
--- portalRredsi/api/appv1/models/rubrica_resultado.py
+++ portalRredsi/api/appv1/models/rubrica_resultado.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Float, Integer
 from models.base_class import Base
 
 class Rubrica_resultado(Base):
-    __tablename__ = 'rubrica_resultado'
+    __tablename__ = 'rubricas_resultados'
     id_rubrica_resultado = Column(Integer, primary_key=True, autoincrement=True)
     puntaje_aprobacion =  Column(Float(2,1))
 
