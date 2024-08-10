d96ac99d12fe35f4acb0ec5fdb637a55d82432b3
Luisa Agudelo <luisaab1103@gmail.com>
2024-08-10 14:34:24 -0500

    Cambios a nombres estandarizados en modelos sala

--- portalRredsi/api/appv1/models/sala.py
+++ portalRredsi/api/appv1/models/sala.py
@@ -4,10 +4,10 @@ from models.base_class import Base
 
 
 class Sala(Base):
-    __tablename__ = "sala"
+    __tablename__ = "salas"
     id_sala = Column(Integer, primary_key=True, autoincrement=True)
-    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'))
-    area_conocimiento = Column(Integer, ForeignKey('area_conocimiento.id_area_conocimiento'))
+    id_usuario = Column(Integer, ForeignKey('usuarios.id_usuario'))
+    area_conocimiento = Column(Integer, ForeignKey('areas_conocimiento.id_area_conocimiento'))
     numero_sala = Column(String(25))
     nombre_sala = Column(String(25))
     
