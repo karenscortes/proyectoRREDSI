0a8c32fdaef14e7e360f4a27ccf57f6dfdfb43e0
Sebastian Usma <johan58usma@gmail.com>
2024-08-24 17:43:27 -0500

    vistas funcionales y reactividad en modal

--- portalRredsi/vistas-vue/src/components/delegado/EvaluadoresRegistrados.vue
+++ portalRredsi/vistas-vue/src/components/delegado/EvaluadoresRegistrados.vue
@@ -51,8 +51,9 @@
                                 </div>
                             </td>
                             <td>
-                                <a href="#" @click="obtenerEvaluadorActual(evaluador)" class="btn-sm font-weight-bold text-dark" type="button"
-                                    data-bs-toggle="modal" data-bs-target="#delegateInformation">
+                                <a href="#" @click="obtenerEvaluadorActual(evaluador)"
+                                    class="btn-sm font-weight-bold text-dark" type="button" data-bs-toggle="modal"
+                                    data-bs-target="#delegateInformation">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
                                         width="24px" fill="#00000">
                                         <path
@@ -69,8 +70,7 @@
             <div aria-label="Page navigation example mb-5">
                 <ul class="pagination justify-content-center">
                     <li class="page-item disabled m-1">
-                        <a class="page-link" href="#" tabindex="-1"
-                            style="border-radius: 20px;">Previous</a>
+                        <a class="page-link" href="#" tabindex="-1" style="border-radius: 20px;">Previous</a>
                     </li>
                     <li class="page-item rounded m-1">
                         <a class="page-link rounded-circle" href="#">1</a>
@@ -93,42 +93,42 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content border border-dark border-5 rounded-5 text-dark">
                     <div class="modal-header text-center">
-                        <h3 class="modal-title mt-5 w-100" id="modalLabel">{{ console.log(evaluadorActual) }}</h3>
+                        <h3 class="modal-title mt-5 w-100" id="modalLabel">{{ evaluadorActual.nombre }}</h3>
                         <button type="button" class="btn-close mr-3 mt-3" data-bs-dismiss="modal"
                             aria-label="Close"></button>
                     </div>
                     <div class="modal-body mt-3">
-                        <div class="row mx-auto justify-content-center">
+                        <div class="row justify-content-center text-start">
                             <div class="col-5 text-dark font-weight-bold px-3">Tipo de Documento:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">Cedula</span>
+                                <span class="text-dark">{{ evaluadorActual.tipo_documento }}</span>
                             </div>
                             <div class="col-5 text-dark font-weight-bold px-3">Documento:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">2102156348</span>
+                                <span class="text-dark">{{ evaluadorActual.identificacion }}</span>
                             </div>
                             <div class="col-5 text-dark font-weight-bold px-3">Nombre Completo:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">Maria Luisa</span>
+                                <span class="text-dark">{{ evaluadorActual.nombre }}</span>
                             </div>
                             <div class="col-5 text-dark font-weight-bold px-3">Área de conocimiento:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">Sistemas</span>
+                                <span class="text-dark">{{ evaluadorActual.areaConocimiento }}</span>
                             </div>
                             <div class="col-5 text-dark font-weight-bold px-3">Institución:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">SENA</span>
+                                <span class="text-dark">{{ evaluadorActual.institucion }}</span>
                             </div>
                             <div class="col-5 text-dark font-weight-bold px-3">Teléfono:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">3204562023</span>
+                                <span class="text-dark">{{evaluadorActual.telefono}}</span>
                             </div>
                             <div class="col-5 text-dark font-weight-bold px-3">Correo:</div>
                             <div class="col-5 border mb-3 px-3">
-                                <span class="text-dark">maria60@gmail.com</span>
+                                <span class="text-dark">{{ evaluadorActual.correo }}</span>
                             </div>
                             <div class="text-center mt-3">
-                                <a href="../delegado/pruebas_documentos/constancia_NotasAprendiz.pdf"
+                                <a :href="evaluadorActual.urlArchivo"
                                     class="btn btn-outline-primary" target="_blank">
                                     Ver Títulos
                                 </a>
@@ -144,39 +144,78 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
+
 export default {
     data() {
         return {
             evaluadores: [
-                {
+                {   
+                    tipo_documento: "Cédula",
                     identificacion: "24644221",
                     nombre: "Sebastian usma",
                     areaConocimiento: "Tecnologia",
                     institucion: "SENA",
-                    estado: true
+                    estado: true,
+                    telefono: "123456789",
+                    correo: "usma@mail.com",
+                    urlArchivo: "ruta/al/archivo.pdf"
                 },
                 {
+                    tipo_documento: "Pasaporte",
                     identificacion: "246415441",
                     nombre: "Luisa Lopez Agudelo",
                     areaConocimiento: "Ciencias naturales",
                     institucion: "SENA",
-                    estado: true
+                    estado: true,
+                    telefono: "666",
+                    correo: "fercha@mail.com",
+                    urlArchivo: "ruta/al/archivo.pdf"
                 },
                 {
+                    tipo_documento: "Pasaporte",
                     identificacion: "104644221",
                     nombre: "Miguel Alzate",
                     areaConocimiento: "Quimica",
                     institucion: "SENA",
-                    estado: false
+                    estado: false,
+                    telefono: "666454545",
+                    correo: "cruelo@mail.com",
+                    urlArchivo: "ruta/al/archivo.pdf"
                 }
-            ]
+            ],
+
         }
     },
-    methods:{
-        obtenerEvaluadorActual(evaluador){
-            const personaActual = evaluador;
-            console.log(personaActual);
-            return { personaActual }
+    setup() {
+        const evaluadorActual = reactive({
+            identificacion: "",
+            nombre: "",
+            areaConocimiento: "",
+            institucion: "",
+            estado: false,
+            telefono: "",
+            correo: "",
+            urlArchivo: ""
+        });
+
+        const obtenerEvaluadorActual = (evaluador) => {
+            evaluadorActual.tipo_documento = evaluador.tipo_documento;
+            evaluadorActual.identificacion = evaluador.identificacion;
+            evaluadorActual.nombre = evaluador.nombre;
+            evaluadorActual.areaConocimiento = evaluador.areaConocimiento;
+            evaluadorActual.institucion = evaluador.institucion;
+            evaluadorActual.estado = evaluador.estado;
+            evaluadorActual.telefono = evaluador.telefono;
+            evaluadorActual.correo = evaluador.correo;
+            evaluadorActual.urlArchivo = evaluador.urlArchivo;
+
+            console.log(evaluadorActual);
+        };
+
+        return {
+            evaluadorActual,
+            obtenerEvaluadorActual
         }
     }
 }
@@ -184,29 +223,29 @@ export default {
 
 <style>
 .section_title h1 {
-	display: block;
-	color: #1a1a1a;
-	font-weight: 500;
-	padding-top: 24px;
+    display: block;
+    color: #1a1a1a;
+    font-weight: 500;
+    padding-top: 24px;
 }
 
 .section_title h1::before {
-	display: block;
-	position: absolute;
-	top: 0;
-	left: 50%;
-	-webkit-transform: translateX(-50%);
-	-moz-transform: translateX(-50%);
-	-ms-transform: translateX(-50%);
-	-o-transform: translateX(-50%);
-	transform: translateX(-50%);
-	width: 55px;
-	height: 4px;
-	content: '';
-	background: #ffb606;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+    -moz-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    -o-transform: translateX(-50%);
+    transform: translateX(-50%);
+    width: 55px;
+    height: 4px;
+    content: '';
+    background: #ffb606;
 }
 
-.page-link{
+.page-link {
     color: black;
 }
 </style>
