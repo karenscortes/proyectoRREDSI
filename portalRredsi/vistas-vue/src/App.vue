5883b0dc3cbef470a701bee4e9f5ec4dbc2b14e5
Laura Motato <morenokatherin988@gmail.com>
2024-08-27 19:37:14 -0500

    Modificacion

--- portalRredsi/vistas-vue/src/App.vue
+++ portalRredsi/vistas-vue/src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <ContainerMain></ContainerMain>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-import ContainerMain from './components/administrador/rubricas/ContainerMain.vue';
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    ContainerMain,
   }
 }
 </script>
