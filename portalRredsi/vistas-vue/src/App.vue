b3ed2622f9d088184a74ef03aaedd97840372a05
Sebastian Usma <johan58usma@gmail.com>
2024-08-28 20:48:03 -0500

    Gestión de horario funcional :)

--- portalRredsi/vistas-vue/src/App.vue
+++ portalRredsi/vistas-vue/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <HorarioEspecifico />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+
 export default {
   name: 'App',
   components: {
