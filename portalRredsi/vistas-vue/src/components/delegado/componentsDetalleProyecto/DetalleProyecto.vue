b140ea79a45a8b199168ea578afe955f5ecf408f
Luisa Agudelo <luisaab1103@gmail.com>
2024-08-31 17:06:28 -0500

    Add: DetalleProyecto

---
+++ portalRredsi/vistas-vue/src/components/delegado/componentsDetalleProyecto/DetalleProyecto.vue
@@ -0,0 +1,254 @@
+<template>
+    <div class="container pt-5">
+        <div class="row mb-4 mt-2">
+            <div class="col">
+                <div class="section_title text-center">
+                    <h1>Detalle del Proyecto</h1>
+                </div>
+            </div>
+        </div>
+        <div class="why-choose-section">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-lg-6 order-2 order-lg-1">
+                        <div class="section-title mt-4 text-left">
+                            <h4>Información del proyecto</h4>
+                        </div>
+                        <div class="row my-5 gy-4">
+                            <!-- Ponentes -->
+                            <div class="col-6 col-md-6">
+                                <PonentesCom :ponentes="ponentes" />
+                            </div>
+                            <!-- Evaluadores -->
+                            <div class="col-6 col-md-6">
+                                <EvaluadoresCom :evaluadores="evaluadores" />
+                            </div>
+                            <!-- Evento -->
+                            <div class="col-6 col-md-6 mt-3">
+                                <EventoCom :horario="horario" :sala="sala" />
+                            </div>
+                            <!-- Suplentes -->
+                            <div class="col-6 col-md-6 mt-3">
+                                <SuplentesCom :tipo="tipo" :suplente="suplente" />
+                            </div>
+                        </div>
+                        <!-- Botones -->
+                        <div class="col-12 d-flex justify-content-between">
+                            <button type="submit" class="btn btn-sm btn-warning font-weight-bold" data-toggle="modal"
+                                data-target="#agregarPresentacion" style="width: 36%;">
+                                Añadir presentación
+                            </button>
+                            <form action="../delegado/pruebas_documentos/constancia_NotasAprendiz.pdf"
+                                style="width: 31%;" target="_blank">
+                                <button type="submit" class="btn btn-sm btn-warning font-weight-bold"
+                                    style="width: 100%;">
+                                    Ver presentación
+                                </button>
+                            </form>
+                            <form action="../delegado/pruebas_documentos/constancia_NotasAprendiz.pdf"
+                                style="width: 31%;" target="_blank">
+                                <button type="submit" class="btn btn-sm btn-warning font-weight-bold"
+                                    style="width: 100%;">
+                                    Ver proyecto
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!-- Imagen del proyecto -->
+                    <div class="col-lg-6 order-1 order-lg-2">
+                        <div class="img-wrap mt-4">
+                            <img src="../../../assets/img/course_5.jpg" style="border-radius: 25px;" alt="Image"
+                                class="img-fluid shadow-lg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Accordion -->
+        <div class="accordion pt-5 mt-3" id="accordionExample">
+            <div class="card p-2">
+                <div id="headingOne">
+                    <button class="btn btn-block toggle-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                        <i style="margin-right: 10px;" class="fa-solid fa-check fa-lg"></i>
+                        Respuesta rúbrica 1
+                    </button>
+                </div>
+                <div id="collapseOne" class="collapse mt-5" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <!-- Componente hijo RubricaCom se monta aquí -->
+                    <RubricaCom :tituloProyecto="tituloProyecto" :ponentesProyecto="ponentesProyecto"
+                        :universidadProyecto="universidadProyecto" :nombreEvaluador="nombreEvaluador"
+                        :cedulaEvaluador="cedulaEvaluador" :universidadEvaluador="universidadEvaluador"
+                        :emailEvaluador="emailEvaluador" :celularEvaluador="celularEvaluador"
+                        :componentes="componentes"/>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import EvaluadoresCom from './EvaluadoresCom.vue';
+import EventoCom from './EventoCom.vue';
+import PonentesCom from './PonentesCom.vue';
+import SuplentesCom from './SuplentesCom.vue';
+import RubricaCom from './RubricaCom.vue';
+
+import { ref } from 'vue';
+
+export default {
+    components: {
+        EvaluadoresCom,
+        EventoCom,
+        PonentesCom,
+        SuplentesCom,
+        RubricaCom,
+    },
+    setup() {
+        // Define variables reactivas con ref o reactive
+        const ponentes = ['Diego Fernando Legarda', 'Felipe Londoño'];
+        const evaluadores = ['Manuel Trujillo Velazques', 'Juan Castillo Montes'];
+        const horario = '10:30 - 11:00 A.M.';
+        const sala = '123ASV';
+        const tipo = 'Evaluador';
+
+        // Datos para RubricaCom
+        const tituloProyecto = ref('BigData');
+        const ponentesProyecto = ref('Sebastian Usma');
+        const universidadProyecto = ref('UTP');
+        const nombreEvaluador = ref('Miguel Alzate');
+        const cedulaEvaluador = ref(123456789);
+        const universidadEvaluador = ref('Catolica');
+        const emailEvaluador = ref('cruelo@mail.com');
+        const celularEvaluador = ref(987654321);
+        const componentes = ref([
+            { titulo: 'Componente 1', valorMaximo: 10, calificacion: 0, observaciones: '' },
+            { titulo: 'Componente 2', valorMaximo: 15, calificacion: 0, observaciones: '' },
+            { titulo: 'Componente 3', valorMaximo: 20, calificacion: 0, observaciones: '' },
+        ]);
+
+        return {
+            ponentes,
+            evaluadores,
+            horario,
+            sala,
+            tipo,
+            tituloProyecto,
+            ponentesProyecto,
+            universidadProyecto,
+            nombreEvaluador,
+            cedulaEvaluador,
+            universidadEvaluador,
+            emailEvaluador,
+            celularEvaluador,
+            componentes,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.section_title h1 {
+    display: block;
+    color: #1a1a1a;
+    font-weight: 500;
+    padding-top: 24px;
+}
+
+.section_title h1::before {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 55px;
+    height: 4px;
+    content: '';
+    background: #ffb606;
+}
+
+.feature {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin-bottom: 15px;
+}
+
+.icon {
+    margin-bottom: 10px;
+}
+
+.toggle-button {
+    text-align: left;
+    padding: 10px 20px;
+    width: 100%;
+}
+
+.card {
+    margin-bottom: 10px;
+}
+
+/* .accordion {
+    display: none;
+} */
+
+.title-line {
+    border-top: 2px solid rgb(255, 182, 6);
+    margin-top: -10px;
+}
+
+.form-section {
+    background-color: #f8f9fa;
+    border: 1px solid #ced4da;
+    padding: 20px;
+    border-radius: 5px;
+    max-width: 800px;
+    margin: auto;
+}
+
+.form-section h2 {
+    color: rgb(255, 182, 6);
+    text-align: center;
+}
+
+.form-section .title-icon {
+    color: rgb(255, 182, 6);
+    margin-right: 10px;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+}
+
+
+
+@media only screen and (max-width:767px) {
+    .big-screen {
+        display: none;
+    }
+
+    .accordion {
+        display: block;
+    }
+
+    .card {
+        width: 100%;
+        text-align: left;
+        text-decoration: none;
+        background-color: #f7f7f7;
+        border: 1px solid black;
+        margin-bottom: 2rem;
+    }
+
+    .card button {
+        font-weight: 700;
+        text-align: center;
+    }
+
+    .toggle-button:not(.collapsed) {
+        color: rgb(255, 182, 6);
+    }
+
+}
+</style>
