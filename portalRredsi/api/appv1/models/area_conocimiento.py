51b0cd80b3fc924995b1d5941796f22cbfecd6df
Juanpa Linares <juampix159@gmail.com>
2024-09-03 09:03:44 -0500

    Resolviendo problemas entorno

--- portalRredsi/api/appv1/models/area_conocimiento.py
+++
@@ -1,9 +0,0 @@
-
-from models.base_class import Base
-from sqlalchemy import Column,Integer, String # type: ignore
-
-class Area_conocimiento(Base):
-    __tablename__ = 'areas_conocimiento'
-    id_area_conocimiento = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String(35))
-    
--- portalRredsi/api/appv1/schemas/evaluador/evaluador.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-from pydantic import BaseModel, StringConstraints
-
-class EtapaBase(BaseModel):
-    nombre: Annotated[str, StringConstraints(max_length=20)]
-
