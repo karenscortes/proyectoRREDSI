5a72c03572e6d6844a5c2f0c501987a7f779eec1
Sebastian Usma <johan58usma@gmail.com>
2024-09-04 00:30:36 -0500

    feat: rutas para asignar proyecto

---
+++ portalRredsi/api/appv1/routers/delegado/asignarProyectoEtapaVirtual.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from appv1.schemas.delegado.asignacionProyectoEtapaVirtual import AsignarProyectoEtapaUno
+from db.database import get_db
+from appv1.crud.delegado.asignarProyectoEtapaVirtual import asignar_proyecto_etapa_virtual, get_convocatoria_actual_por_proyecto
+
+router_proyecto_etapa_uno = APIRouter()
+
+@router_proyecto_etapa_uno.post("/asignar-proyecto-etapa-uno")
+async def asignar_proyecto_etapa_uno(
+    asignacion: AsignarProyectoEtapaUno,
+    db: Session = Depends(get_db)
+):
+    respuesta = asignar_proyecto_etapa_virtual(db, asignacion)
+    if respuesta:
+        return {"mensaje":f"Proyecto asignado a {asignacion.id_datos_personales} "}
+    else:
+        return {"mensaje":"El proyecto no se ha podido asignar con exito"}
+
+
+@router_proyecto_etapa_uno.get("/obtener-proyecto-convocatoria", response_model=dict)
+async def buscar_convocatoria_por_proyecto(
+    id_proyecto: int,
+    db: Session = Depends(get_db)
+):
+    proyecto_convocatoria = get_convocatoria_actual_por_proyecto(db, id_proyecto)
+    
+    if proyecto_convocatoria:
+        return {
+            "proyecto_convocatoria": proyecto_convocatoria._asdict()
+        }
+    else:
+        return {"mensaje":"El proyecto no se ha podido encontrar en una convocatoria vigente"}
--- portalRredsi/api/appv1/routers/delegado/salas.py
+++ portalRredsi/api/appv1/routers/delegado/salas.py
@@ -5,12 +5,12 @@ from appv1.schemas.delegado.salas import AsignarProyectoSala, DetalleSala, SalaR
 from db.database import get_db
 from appv1.crud.delegado.salas import asignar_proyecto_a_sala, get_detalle_sala, get_salas, get_salas_por_convocatoria
 
-router_delegado = APIRouter()
+router_sala = APIRouter()
 
 
 # < RUTAS SALAS
 
-@router_delegado.post("/asignar-proyecto")
+@router_sala.post("/asignar-proyecto")
 async def asignar_proyecto(asignacion: AsignarProyectoSala, db: Session = Depends(get_db)):
     respuesta = asignar_proyecto_a_sala(db, asignacion)
     if respuesta:
@@ -19,28 +19,38 @@ async def asignar_proyecto(asignacion: AsignarProyectoSala, db: Session = Depend
         return {"mensaje":"El proyecto no se ha podido asignar con exito"}
 
 
-@router_delegado.get("/get-all-salas/", response_model=List[SalaResponse])
+@router_sala.get("/get-all-salas/", response_model=List[SalaResponse])
 async def read_all_salas(
     db: Session = Depends(get_db)
 ):
     salas = get_salas(db)
     if len(salas) == 0:
-        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+        raise HTTPException(status_code=404, detail="Salas no encontradas")
     return salas
 
-@router_delegado.get("/get-salas-por-convocatoria/", response_model=dict)
-async def read_all_salas_por_categoria(
+@router_sala.get("/get-salas-por-convocatoria/", response_model=dict)
+async def read_all_salas_por_convocatoria(
+    page: int = 1,
+    page_size: int = 10,
     db: Session = Depends(get_db)
 ):
-    salas = get_salas_por_convocatoria(db)
-    if len(salas) == 0:
-        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    salas, total_pages = get_salas_por_convocatoria(db, page, page_size)
     
-    response = {"salas": [sala._asdict() for sala in salas]}
-    return response
+    if len(salas) == 0:
+        raise HTTPException(status_code=404, detail="Salas no encontradas")
+    
+    # Convertir cada fila en un diccionario
+    salas_convocatoria = [dict(sala) for sala in salas]
+
+    return {
+        "salas": salas_convocatoria,
+        "total_pages": total_pages,
+        "current_page": page,
+        "page_size": page_size
+    }
 
 
-@router_delegado.get("/get-detalle-sala/", response_model=DetalleSala)
+@router_sala.get("/get-detalle-sala/", response_model=DetalleSala)
 async def read_detalle_sala(
     id_sala: str,
     db: Session = Depends(get_db),
