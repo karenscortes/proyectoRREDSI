fba22d322884f8cd65e7a254720af078b5fd2373
Juanpa Linares <juampix159@gmail.com>
2024-09-03 22:21:33 -0500

    Modulo SuperAdmin completo

--- portalRredsi/api/main.py
+++ portalRredsi/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from appv1.routers import usuarios, rol
 from appv1.routers.evaluador import evaluadores
+from appv1.routers.superadmin import superadmin
 from db.database import test_db_connection
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -9,6 +10,8 @@ app = FastAPI()
 app.include_router(usuarios.router_user, prefix="/users", tags=["Usuarios"])
 app.include_router(rol.router_rol, prefix="/roles", tags=["Roles"])
 app.include_router(evaluadores.router_evaluador, prefix="/proyectos", tags=["Evaluadores"])
+app.include_router(superadmin.router_superadmin, prefix="/superadmin", tags=["SuperAdmin"])
+
 
 def on_startup():
     test_db_connection()
