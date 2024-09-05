7f0b3b60d3b63202f2809745e727415ac0bd1d04
Maribel Obando <maribel-obando.j@utp.edu.co>
2024-09-05 16:36:14 -0500

    api para lista de evaluadores

--- portalRredsi/api/appv1/routers/delegado/listaEvaluadores.py
+++ portalRredsi/api/appv1/routers/delegado/listaEvaluadores.py
@@ -3,26 +3,27 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from appv1.schemas.usuario import UserResponse
 from db.database import get_db
-from appv1.crud.delegado.postulaciones import get_all_applications, get_convocatoria
+from delegado.listaEvaluadores import get_all_evaluators, get_id_rol, update_evaluator_status
 
-router_postulaciones = APIRouter()
-MODULE = 'postulaciones_evaluadores'
+router_evaluadores = APIRouter()
+MODULE = 'usuarios'
 
-@router_postulaciones.get("/get-all-applications/", response_model=List[UserResponse])
-async def read_all_applications(
+@router_evaluadores.get("/get-all-evaluators/", response_model=List[UserResponse])
+async def read_all_evaluators(
     db: Session = Depends(get_db)
     #current_user: UserResponse = Depends(get_current_user)
 ):
     # permisos = get_permissions(db, current_user.user_role, MODULE)
     # if not permisos.p_select:
     #     raise HTTPException(status_code=401, detail="Usuario no autorizado")
-    id_convocatoria = get_convocatoria(db)
-    applications = get_all_applications(db, id_convocatoria)
-    if len(applications) == 0:
-        raise HTTPException(status_code=404, detail="No hay postulaciones")
-    return applications
+    id_rol = get_id_rol(db,'Evaluador')
+    evaluadores = get_all_evaluators(db, id_rol)
+    if len(evaluadores) == 0:
+        raise HTTPException(status_code=404, detail="No hay Evaluadores")
+    return evaluadores
 
-@router_postulaciones.put("/update-application-status/", response_model=dict)
+
+@router_evaluadores.put("/update-evaluator-status/", response_model=dict)
 def update_application_status(
     id_evaluador:str,
     estado: str,
@@ -33,10 +34,10 @@ def update_application_status(
     # if user_id != current_user.user_id:
     #     if not permisos.p_update:
     #         raise HTTPException(status_code=401, detail="Usuario no autorizado")
-    id_convocatoria = get_convocatoria(db)
+
     # verify_evaluator = get_user_by_id(db, id_evaluador)
     # if verify_evaluator is None:
     #     raise HTTPException(status_code=404, detail="Evaluador no encontrado")     
-    updated= update_application_status(db, id_convocatoria, id_evaluador,estado)
+    updated= update_evaluator_status(db,id_evaluador,estado)
     if updated:
         return {"mensaje": "Postulación procesada con exito" }
