7a97874a65980219f1f59207ef3ca823a9dc1140
Miguel-Alzate <miguelangel4817@gmail.com>
2024-09-08 17:34:59 -0500

    cambios modelos

--- portalRredsi/api/appv1/models/rubrica_resultado.py
+++ portalRredsi/api/appv1/models/rubrica_resultado.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Float, Integer
+from sqlalchemy import DECIMAL, Column, Integer
 from .base_class import Base
 
 class Rubrica_resultado(Base):
     __tablename__ = 'rubricas_resultados'
     id_rubrica_resultado = Column(Integer, primary_key=True, autoincrement=True)
-    puntaje_aprobacion =  Column(Float(2,1))
+    puntaje_aprobacion =  Column(DECIMAL(3,1))
 
