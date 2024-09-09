d0605b483f40c5fd3bf13f5b7ea2e8e0fed53d47
Valeria Carvajal <vacarvajalo@gmail.com>
2024-09-09 18:30:40 -0500

    login vue

--- portalRredsi/FrontVue/src/services/api.js
+++ portalRredsi/FrontVue/src/services/api.js
@@ -11,32 +11,32 @@ const api = axios.create({
   }
 });
 
-// Interceptor para añadir el token de autorización
-// api.interceptors.request.use(config => {
-//     const authStore = useAuthStore(); // Obtener el store de autenticación
-//     const token = authStore.accessToken;
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`; // Añadir el token a las cabeceras
-//     }
-//     return config;
-//   }, error => {
-//     return Promise.reject(error);
-//   });
+//Interceptor para añadir el token de autorización
+api.interceptors.request.use(config => {
+    const authStore = useAuthStore(); // Obtener el store de autenticación
+    const token = authStore.accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Añadir el token a las cabeceras
+    }
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  });
 
 
-// Interceptor para manejar errores
-// api.interceptors.response.use(response => {
-//   return response;
-// }, error => {
-//   if (error.response && error.response.status === 401 && error.response.data.detail === 'Invalid token') {
-//     // Elimina el token y los datos de usuario del store
-//     const authStore = useAuthStore();
-//     authStore.logout();
-//     // Redirigir a la página de login
-//     router.push('/'); 
-//   }
-//   return Promise.reject(error);
-// });
+//Interceptor para manejar errores
+api.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response && error.response.status === 401 && error.response.data.detail === 'Invalid token') {
+    // Elimina el token y los datos de usuario del store
+    const authStore = useAuthStore();
+    authStore.logout();
+    // Redirigir a la página de login
+    router.push('/'); 
+  }
+  return Promise.reject(error);
+});
 
 
 export default api;
