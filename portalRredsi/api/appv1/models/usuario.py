981f1cdb8db15b910ef34e895af1b7929a03f394
Valeria Carvajal <vacarvajalo@gmail.com>
2024-09-08 22:59:45 -0500

    login

--- portalRredsi/api/appv1/models/usuario.py
+++ portalRredsi/api/appv1/models/usuario.py
@@ -9,14 +9,17 @@ class Estados(PyEnum):
     pendiente = "Pendiente"
 
 class Usuario(Base):
-    __tablename__ = 'usuarios'
+    _tablename_ = 'usuarios'
     id_usuario = Column(Integer, primary_key=True, autoincrement=True)
     id_rol = Column(Integer, ForeignKey('roles.id_rol'))
+    id_tipo_documento = Column(Integer, ForeignKey('tipos_documento.id_tipo_documento'))
+    documento = Column(String(55))
+    nombres = Column(String(25))
+    apellidos = Column(String(25))
+    celular = Column(String(10))
     correo = Column(String(70), unique=True)
     clave = Column(String(255))
     estado = Column(Enum(Estados), default=Estados.activo)
    
-    rol = relationship("Rol", back_populates="usuarios")
-    detalle_personal = relationship("Detalle_personal",back_populates="usuario")
-    postulaciones_evaluadores = relationship("Postulacion_evaluador", back_populates="evaluador")
-    salas = relationship("Sala", back_populates="usuario")
+    tipo_documento = relationship("Tipo_documento")
+    rol = relationship("Rol")
