0fcbdb7b12b4ae21ae463ab6a8387ecad26ba9f3
Laura Motato <morenokatherin988@gmail.com>
2024-09-10 06:30:58 -0500

    avance

---
+++ portalRredsi/api/appv1/schemas/titulo_academico.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+from pydantic import BaseModel, StringConstraints
+import enum
+
+class NivelEnum(str, enum.Enum):
+    pregrado = "pregrado"
+    maestria = "maestria"
+    especializacion = "especializacion"
+    doctorado = "doctorado"
+
+
+class TituloAcademicoBase(BaseModel):
+    nivel: NivelEnum
+    nombre_titulo: Annotated[str, StringConstraints(max_length=80)]
+    url_titulo: Annotated[str, StringConstraints(max_length=255)]
+    class Config:
+        orm_mode = True 
+
+
+class TituloAcademicoResponse(TituloAcademicoBase):
+    id_titulo_academico: int
+    
+    
