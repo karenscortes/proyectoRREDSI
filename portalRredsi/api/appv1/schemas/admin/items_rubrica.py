6e4b81131c62727fd88707642401c6cedd2a15ad
Laura Motato <morenokatherin988@gmail.com>
2024-09-11 05:17:27 -0500

    Avance en los endPoints

--- portalRredsi/api/appv1/schemas/admin/items_rubrica.py
+++ portalRredsi/api/appv1/schemas/admin/items_rubrica.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import BaseModel, StringConstraints, condecimal 
 class ItemRubrica(BaseModel):
     titulo: Annotated[str, StringConstraints(max_length=50)]
@@ -13,7 +13,12 @@ class ItemRubricaResponse(ItemRubrica):
     class Config:
         orm_mode = True 
 
-
 class ItemCreate(ItemRubrica):
     pass
-    
+
+class ItemUpdate(BaseModel):
+    titulo: Optional[Annotated[str, StringConstraints(max_length=50)]]
+    componente: Optional[str]
+    valor_max: Optional[Annotated[float,condecimal(max_digits=2, decimal_places=1)]]
+    class Config:
+        orm_mode = True
