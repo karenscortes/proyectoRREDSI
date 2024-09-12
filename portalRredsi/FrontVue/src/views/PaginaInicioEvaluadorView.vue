153f935f4c47754e1008b440a39c2cd449cd239a
Miguel-Alzate <miguelangel4817@gmail.com>
2024-09-12 09:18:58 -0500

    avances consumo postulacion

--- portalRredsi/FrontVue/src/views/PaginaInicioEvaluadorView.vue
+++ portalRredsi/FrontVue/src/views/PaginaInicioEvaluadorView.vue
@@ -6,14 +6,14 @@
                     <div class="become_title">
                         <h1>Bienvenido al portal RREDSI</h1>
                     </div>
-                    <p class="become_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Etiam eu purus nec eros varius luctus. Praesent finibus risus facilisis ultricies venenatis. Suspendisse fermentum sodales lacus, lacinia gravida elit dapibus sed. Cras in lectus elit. Maecenas tempus nunc vitae mi egestas venenatis. Aliquam rhoncus, purus in vehicula porttitor, lacus ante consequat purus, id elementum enim purus nec enim. In sed odio rhoncus, tristique ipsum id, pharetra neque.</p>
+                    <p class="become_text">RREDSI es una red reconocida por su impacto en el desarrollo de proyectos de innovación y tecnología. A lo largo de los años, nuestros evaluadores han desempeñado un rol clave en garantizar la calidad y relevancia de los proyectos presentados. Su capacidad para analizar y proporcionar retroalimentación precisa ha contribuido significativamente al éxito de cada convocatoria. Los logros obtenidos demuestran el compromiso y la excelencia en cada evaluación realizada. RREDSI sigue avanzando como un motor de progreso para la ciencia y la innovación en Colombia. ¿Que esperas para unirte?</p>
                     <div class="become_button text-center trans_200">
-                        <a href="#">Explorar</a>
+                        <a href="#">Postularme</a>
                     </div>
                 </div>
                 <div class="col-lg-6 order-1 order-lg-2">
                     <div class="become_image">
-                        <img src="../plantilla/images/become.jpg" alt="img">
+                        <img src="../assets/img/become.jpg" alt="img">
                     </div>
                 </div>
                 
@@ -22,9 +22,52 @@
     </div>
 </template>
 
-<script>    
+<script>
+    import { insertarPostulacionEvaluador } from '../services/evaluadorService'; 
+    import { useAuthStore } from '@/store';
 
+    export default {
+        data() {
+            return {
+                id_evaluador: null,      
+                etapa_virtual: 0,       
+                etapa_presencial: 0,
+                jornada_manana: 0,
+                jornada_tarde: 0,
+                postulacionExitosa: false 
+            };
+        },
+        methods: {
+            async insertarPostulacion() {
+                try {
+                    const authStore = useAuthStore();
+                    const user = authStore.user;
+
+                    const postulacionData = {
+                        id_evaluador: user.id_usuario,  
+                        etapa_virtual: this.etapa_virtual,
+                        etapa_presencial: this.etapa_presencial,
+                        jornada_manana: this.jornada_manana,
+                        jornada_tarde: this.jornada_tarde
+                    };
+
+                    const response = await insertarPostulacionEvaluador(postulacionData);
+                    console.log('Postulación exitosa', response.data);
+
+                    this.postulacionExitosa = true;  
+                } catch (error) {
+                    console.error('Error al insertar postulación:', error.message);
+                    alert('Error al insertar postulación');
+                }
+            },
+        },
+        mounted() {
+            const authStore = useAuthStore();
+            this.id_evaluador = authStore.user.id_usuario;  
+        }
+    };
 </script>
+
 
 <style scoped> 
     .become
