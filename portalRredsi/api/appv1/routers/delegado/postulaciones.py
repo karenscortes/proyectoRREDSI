2526dc9030a9f9b368fda1dd7a3d7d9778fdbf54
Maribel Obando <maribel-obando.j@utp.edu.co>
2024-09-16 16:16:39 -0500

    aplicando permisos a rutas

--- portalRredsi/api/appv1/routers/delegado/postulaciones.py
+++ portalRredsi/api/appv1/routers/delegado/postulaciones.py
@@ -1,24 +1,29 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from appv1.routers.login import get_current_user
 from appv1.schemas.delegado.postulaciones import PaginatedApplications, certificatesResponse
+from appv1.schemas.usuario import UserResponse
 from db.database import get_db
 from appv1.crud.delegado.postulaciones import get_all_applications, get_certificates_by_id, update_application_status
+from appv1.crud.permissions import get_permissions
+from appv1.crud.usuarios import get_user_by_id
 
 router_postulaciones = APIRouter()
-MODULE = 'postulaciones_evaluadores'
+MODULE = 8
 
 @router_postulaciones.get("/get-all-applications/", response_model=PaginatedApplications)
 async def read_all_applications(
     page: int = 1,
     page_size: int = 10,
-    db: Session = Depends(get_db)
-    #current_user: UserResponse = Depends(get_current_user)
+    db: Session = Depends(get_db),
+    current_user: UserResponse = Depends(get_current_user)
 ):
-    # permisos = get_permissions(db, current_user.user_role, MODULE)
-    # if not permisos.p_select:
-    #     raise HTTPException(status_code=401, detail="Usuario no autorizado")
+    permisos = get_permissions(db, current_user.id_rol, MODULE)
+    if not permisos.p_consultar:
+        raise HTTPException(status_code=401, detail="Usuario no autorizado")
     applications,total_pages = get_all_applications(db, page, page_size)
+
     return {
         "applications": applications,
         "total_pages": total_pages,
@@ -29,12 +34,8 @@ async def read_all_applications(
 @router_postulaciones.get("/get-certificates-by-id", response_model=List[certificatesResponse])
 async def read_all_certificates(
     id_usuario:int,
-    db: Session = Depends(get_db)
-    #current_user: UserResponse = Depends(get_current_user)
+    db: Session = Depends(get_db) 
 ):
-    # permisos = get_permissions(db, current_user.user_role, MODULE)
-    # if not permisos.p_select:
-    #     raise HTTPException(status_code=401, detail="Usuario no autorizado")
     certificates = get_certificates_by_id(db, id_usuario)
     if len(certificates) == 0:
         raise HTTPException(status_code=404, detail="No hay titulos académicos")
@@ -45,16 +46,16 @@ def update_status(
     id_evaluador:int,
     estado: str,
     db: Session = Depends(get_db),
-    #current_user: UserResponse = Depends(get_current_user)
+    current_user: UserResponse = Depends(get_current_user)
 ):
-    # permisos = get_permissions(db, current_user.user_role, MODULE)
-    # if user_id != current_user.user_id:
-    #     if not permisos.p_update:
-    #         raise HTTPException(status_code=401, detail="Usuario no autorizado")
+    permisos = get_permissions(db, current_user.id_rol, MODULE)
+    if id_evaluador != current_user.id_usuario:
+        if not permisos.p_actualizar:
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
 
-    # verify_evaluator = get_user_by_id(db, id_evaluador)
-    # if verify_evaluator is None:
-    #     raise HTTPException(status_code=404, detail="Evaluador no encontrado")     
+    verify_evaluator = get_user_by_id(db, id_evaluador)
+    if verify_evaluator is None:
+        raise HTTPException(status_code=404, detail="Evaluador no encontrado")     
     updated= update_application_status(db,id_evaluador,estado)
     if updated:
-        return {"mensaje": "Postulación procesada con exito" }
+        return {"mensaje": "El estado de la Postulación fue modificado con éxito" }
