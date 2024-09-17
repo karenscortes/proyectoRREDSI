856a210584826b109ee6daef6a91601bfd6a256a
Maribel Obando <maribel-obando.j@utp.edu.co>
2024-09-17 07:09:40 -0500

    pruebas y arreglos en apis

--- portalRredsi/api/appv1/crud/delegado/listaEvaluadores.py
+++ portalRredsi/api/appv1/crud/delegado/listaEvaluadores.py
@@ -104,4 +104,36 @@ def get_evaluator_by_document(db: Session, documento: str):
         print(f"Error al obtener evaluador por documento: {e}")
         raise HTTPException(status_code=500, detail="Error al obtener evaluador por documento")
 
+def get_evaluator_by_id(db: Session, id_evaluador: int):
+    try:
+        sql = text(
+        """
+            SELECT usuarios.id_usuario,
+            usuarios.correo,
+            usuarios.estado,
+            usuarios.nombres,
+            usuarios.apellidos,
+            usuarios.celular,
+            instituciones.nombre AS nombre_institucion,
+            area1.nombre         AS area_conocimiento,
+            area2.nombre         AS otra_area
+            FROM usuarios
+                INNER JOIN detalles_institucionales ON (usuarios.id_usuario = detalles_institucionales.id_usuario)
+                INNER JOIN instituciones ON (detalles_institucionales.id_institucion = instituciones.id_institucion)
+                LEFT JOIN areas_conocimiento area1 ON (detalles_institucionales.id_primera_area_conocimiento = area1.id_area_conocimiento)
+                LEFT JOIN areas_conocimiento area2 ON (detalles_institucionales.id_segunda_area_conocimiento = area2.id_area_conocimiento)
+            WHERE usuarios.id_usuario = :id_evaluador AND usuarios.id_rol IN (
+                SELECT id_rol
+                FROM roles
+                WHERE nombre = 'Evaluador'
+            )
+        """
+        )
+
+        result = db.execute(sql, {"id_evaluador": id_evaluador}).fetchone()
+        return result
+    except SQLAlchemyError as e:
+        print(f"Error al obtener evaluador por id: {e}")
+        raise HTTPException(status_code=500, detail="Error al obtener evaluador por id")
+
 
